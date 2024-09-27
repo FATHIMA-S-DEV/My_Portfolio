6fad88d91fc018dc09931b7ad209e1bb89f840bf
my pc <fathimas.ug22.cs@francisxavier.ac.in>
2024-09-27 15:45:20 +0530

    initial commit

---
+++ src/componenet/footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./footer.css"
+const footer = () => {
+  return (
+    <div className='footer'>
+        <div className='rightsection'>
+            <ul>
+                <li>+91 5678454564</li>
+                <li>ss@gmail.com</li>
+            </ul>
+
+        </div>
+        <div className='leftsection'>
+            <h3>Manakavalam pillai hospital<br/>Palayamkottai<br/></h3>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default footer
---
+++ src/componenet/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './navbar.css';
+
+const navbar = () => {
+  return (
+    <>
+    <header className='port'>Portfolio...</header>
+    <header className='nav'>
+        <ul className='navigation'>
+           <li><a href="home">HOME</a></li>
+           <li><a href="home">ABOUT</a></li>
+           <li><a href="home">CONTACT</a></li>
+           <li><a href="home">PROJECT</a></li>
+
+        </ul>
+    </header>
+ 
+    </>
+  )
+}
+
+export default navbar;
